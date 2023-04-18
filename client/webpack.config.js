1585c10a9d1982942b85fd9fc5a5d28282b012b9
PintoDrop <mikebender22@gmail.com>
2023-04-18 15:14:10 -0700

    test

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,6 +23,28 @@ module.exports = () => {
 
     module: {
       rules: [
+        // is this right?
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg}jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+        // install babel loader
+        // git pushing issues?
+        
         
       ],
     },
