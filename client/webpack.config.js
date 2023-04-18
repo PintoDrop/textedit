730086c0202017215014260d1a7f7345a70b3949
PintoDrop <mikebender22@gmail.com>
2023-04-18 15:59:36 -0700

    adding scripts to npm run start

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,59 +8,62 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
+        template: "./index.html",
+        title: "JATE",
       }),
 
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
 
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'JATE',
-      })
-      
+        name: "JATE",
+        description: "Just Another Text Editor",
+        background_color: "#225ca3",
+        them_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 129, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        // is this right?
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg}jpeg|gif)$/i,
-          type: 'asset/resource',
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
-        // install babel loader
-        // git pushing issues?
-        
-        
+              presets: ["@babel/preset-env"],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
