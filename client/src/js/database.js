ce1f7573ffce7bc71a725969ce4ca3401e9ea71f
PintoDrop <mikebender22@gmail.com>
2023-04-19 13:28:56 -0700

    test push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,7 +4,7 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+        console.log('jate database already exists!');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
@@ -13,27 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => 
-{
+export const putDb = async (content) => {
 // console.error('putDb not implemented');
 // 
-  console.log("PUT has been added to the DB");
- const jateDb = await openDB("jate", 1);
- const tx = jateDb.transaction("jate", "readwrite");
- const store = tx.objectStore("jate");
- const request = store.put({ id: 1, value: content })
- const result = await request;
- console.log("Data has been saved in the database", result.value);
+  console.log("PUT to the DB");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content })
+  const result = await request;
+  console.log("Data has been saved in the database! Hooray!", result.value);
 };
 
 // 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => 
-{
+export const getDb = async () => {
   // console.error('getDb not implemented');
   // 
-  console.log("GET has been added to the DB");
+  console.log("GET from the DB");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
